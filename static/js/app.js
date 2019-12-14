fdec0ee25fbd82a9d01145646391faccc84fbb07
Caleb <calebchronowski@gmail.com>
2019-12-14 02:45:02 -0600

    this has the months in sequential order and functioning javascript to generate the main scatterplot

--- static/js/app.js
+++ static/js/app.js
@@ -30,42 +30,42 @@ function buildCharts(state) {
   // Use `d3.json` to fetch the state data for the plots
   const url = "/state_data/" + state;
   d3.json(url).then(data => {
-    console.log("Data: ", data);
-  
+    console.log("Data: ", data);  
     let dictLength = Object.keys(data).length
-
-    // Turn the big dictionary into the lists that plotly needs for inputs
+    
+  // Turn the big dictionary into the lists that plotly needs for inputs
     var date = [];
+    var index = [];
     var number = [];
     var i = 0;
-
-    for (var i = 0; i < dictLength; i++)
-    {
-      year.push(data[i]["date"]);
+    
+    for (var i = 0; i < dictLength; i++){
+      date.push(data[i]["date"]);
+      index.push(i);
       number.push(data[i]["number"]);
     }     
-
-  //the keys with only their top 10 entries (the data was sorted in python before being served)
     
-    // let otu_ids = samples["otu_ids"];
-    // let sample_values = samples["sample_values"];
-    // let otu_labels = samples["otu_labels"];
-
-    // let trace1 = {
-    //   labels: otu_ids.slice(0, 10),
-    //   values: sample_values.slice(0, 10),
-    //   hovertext: otu_labels.slice(0, 10),
-    //   type: "pie"
-    // };
-
-    // let pie_data = [trace1];
-    // Plotly.newPlot("pie", pie_data);
-
+  //the keys with only their top 10 entries (the data was sorted in python before being served)
+        
+        // let otu_ids = samples["otu_ids"];
+         // let sample_values = samples["sample_values"];
+        // let otu_labels = samples["otu_labels"];
+    
+        // let trace1 = {
+        //   labels: otu_ids.slice(0, 10),
+        //   values: sample_values.slice(0, 10),
+        //   hovertext: otu_labels.slice(0, 10),
+        //   type: "pie"
+        // };
+    
+        // let pie_data = [trace1];
+        // Plotly.newPlot("pie", pie_data);
+    
     let trace2 = {
-      x: date,
+      x: index,
       y: number,
       color: "red",
-      text: state,
+      text: date,
       mode: "markers",
       marker: {
         opacity: 0.3,
@@ -75,13 +75,13 @@ function buildCharts(state) {
     let bubble_data = [trace2];
     Plotly.newPlot("bubble", bubble_data);
   });
-
-    // @TODO: Build a Bubble Chart using the sample data
-
-    // @TODO: Build a Pie Chart
-
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).
+    
+        // @TODO: Build a Bubble Chart using the sample data
+    
+        // @TODO: Build a Pie Chart
+    
+        // HINT: You will need to use slice() to grab the top 10 sample_values,
+        // otu_ids, and labels (10 each).
 };
 
 function init() {
